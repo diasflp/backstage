852abf5df7f9959199d4e4bacf72a1c10542ab52
Felipe Dias <flp.magno@gmail.com>
2019-09-04 14:48:44 -0300

    add environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
+import { environment } from './environment';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://dias:senha@cluster0-tol24.mongodb.net/backStage?retryWrites=true&w=majority',
+      `mongodb+srv://dias:${
+        environment.senhaDB
+      }@cluster0-tol24.mongodb.net/backStage?retryWrites=true&w=majority`,
       { useNewUrlParser: true, useCreateIndex: true },
     ),
     CustomerModule,
