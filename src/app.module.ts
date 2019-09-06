c151504738c9f1101fde720354af3ea016050553
Felipe Dias <flp.magno@gmail.com>
2019-09-06 10:40:04 -0300

    config env database

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,10 @@ import { AuthModule } from './customer/auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      `mongodb+srv://dias:${
-        environment.senhaDB
-      }@cluster0-tol24.mongodb.net/backStage?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useCreateIndex: true },
-    ),
+    MongooseModule.forRoot(environment.database, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }),
     CustomerModule,
     AuthModule,
   ],
